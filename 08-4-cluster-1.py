e283b36980c1c5c7c93a14ee20982b0b09f67178
cyrilvincent <contact@cyrilvincent.com>
2019-10-02 10:16:41 +0200

    Remove DL

--- 08-4-cluster-1.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-cluster = tf.train.ClusterSpec({"myjob": ["localhost:2222", "localhost:2223"]})
-server = tf.train.Server(cluster, job_name="myjob", task_index=0)
-server.join()
-# rpc://job:myjob/task:0
-
