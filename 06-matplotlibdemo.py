9255970ae03b0dc47513d071a32ef54b5ad553c9
cyrilvincent <contact@cyrilvincent.com>
2019-10-14 15:00:09 +0200

    ATP 0.75/3

--- 06-matplotlibdemo.py
+++ 06-matplotlibdemo.py
@@ -1,18 +1,13 @@
 import matplotlib.pyplot as plt
-import math
-lx = range(1000)
-
-def sinx(x):
-    return math.sin(x) / 100
-
-ly = list(map(sinx, lx))
-
-ly = [math.sin(x / 100) * 1000 for x in lx]
-plt.plot(lx, ly)
-plt.show()
 
 import csv
 with open("house/house.csv") as f:
     reader = csv.DictReader(f)
+    surfaces=[]
+    loyers=[]
     for row in reader:
-        print(int(row["loyer"]) / int(row["surface"]))
+        surfaces.append(int(row["surface"]))
+        loyers.append((int(row["loyer"])))
+
+plt.scatter(surfaces, loyers)
+plt.show()
