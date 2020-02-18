871c4852781a5c860deef4b60d30f16d9d5edd2a
contact@cyrilvincent.com <bi$olin38>
2020-02-18 09:43:06 +0100

    ML 1.25/3

--- pandasdemo.py
+++ pandasdemo.py
@@ -2,11 +2,27 @@ import pandas as pd
 import numpy as np
 
 dataframe = pd.read_csv("data/house/house.csv")
-dataframe = dataframe[dataframe.surface < 250]
-
-import sqlite3
-with sqlite3.connect("data/house/house.db3") as conn:
-    dataframe = pd.read_sql("select * from house", conn)
 
 res = dataframe.loyer / dataframe.surface
-print(np.mean(res), np.std(res))
+
+
+dataframe = dataframe[dataframe.surface < 300]
+
+mean = np.mean(res)
+std = np.std(res)
+
+dataframe = dataframe[abs(dataframe.loyer / dataframe.surface - mean) < 3 * std]
+
+
+import scipy.stats as stats
+slope, intercept, rvalue, pvalue, error = stats.linregress(dataframe.surface, dataframe.loyer)
+print(slope, intercept, rvalue, pvalue, error)
+
+import matplotlib.pyplot as plt
+plt.scatter(dataframe.surface, dataframe.loyer)
+plt.plot(dataframe.surface, slope * dataframe.surface + intercept, color="red")
+plt.show()
+
+
+
+
