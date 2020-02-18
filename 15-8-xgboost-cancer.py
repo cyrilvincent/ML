39a6ae3c434c17edd2d79627030960d4fc1c2eca
contact@cyrilvincent.com <bi$olin38>
2020-02-18 15:44:39 +0100

    ML 1.75/3

--- 15-8-xgboost-cancer.py
+++ 15-8-xgboost-cancer.py
@@ -12,7 +12,7 @@ from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test = train_test_split(X,y)
 
 import xgboost as xgb
-model = xgb.XGBClassifier()
+model = xgb.XGBClassifier(n_estimators=100)
 model.fit(X_train, y_train)
 
 score = model.score(X_test, y_test)
