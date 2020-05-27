d1ddf3aabd53497fdb43878e889c5189554e14b9
cyrilvincent <contact@cyrilvincent.com>
2020-05-27 13:08:12 +0200

    Skema 0.5/3

---
+++ tpscipy.py
@@ -0,0 +1,8 @@
+# Installer scipy
+# Calculer la régression linéaire surfaces / loyers
+# surfaces = surfaces[surfaces < 200] recalcul
+# Charger covid-france.txt er créer les tableaux nbcas, dcs
+# Afficher dans un graphique en barre (bar)
+# mean, max, min, std
+# Calculer la régression linéaire et voir qu'elle ne marche pas
+# Facultatif : calculer la régression linéaire sur t < 54
