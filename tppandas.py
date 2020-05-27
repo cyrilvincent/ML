111a643137aca6ea80597f27a5f3f4adf9d67214
cyrilvincent <contact@cyrilvincent.com>
2020-05-27 16:49:13 +0200

    Skema 1/3

---
+++ tppandas.py
@@ -0,0 +1,7 @@
+# Installer pandas
+# Import house.csv
+# Refaire l'exercice précédent et retrouver les mêmes data
+# Importer wdbc en s'inspirant de 12-05-pandas-cancer
+# Calculer le rayon des cellules X[:,0] calculer moyenne ecart type
+# Calculer moyenne ecart type pour les cellules cancereuses
+# Calculer moyenne ecart type pour les cellules saines
