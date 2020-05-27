9c76959e0615e0a4f957ef8a0f448240b80cc740
cyrilvincent <contact@cyrilvincent.com>
2020-05-27 12:24:25 +0200

    Skema 0.5/3

---
+++ tpnumpy.py
@@ -0,0 +1,4 @@
+# Installer numpy
+# Créer le tableau loyers et surfaces
+# Créer le tableau loyerperm2 = loyers / surfaces
+# Calculer min ,max, moyenne, ecart-type, median, normalisations = (x - moyenne) / std
