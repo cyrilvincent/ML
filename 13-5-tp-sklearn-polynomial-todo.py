511a53fd632e2a9c62613cbef802627c6ef8220b
cyrilvincent <contact@cyrilvincent.com>
2020-05-28 10:54:39 +0200

    Skema 1.25/3

---
+++ 13-5-tp-sklearn-polynomial-todo.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import sklearn.linear_model as sklm
+
+data = pd.read_csv('data/house/house.csv')
+model = sklm.LinearRegression()
+x = data["surface"].values.reshape(-1,1)
+y = data["loyer"]
+
+import matplotlib.pyplot as plt
+plt.scatter(x, y)
+plt.show()
