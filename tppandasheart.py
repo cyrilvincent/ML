abd94a0a88f7d75dc43aba4b8ca6dc2f2443db4b
cyrilvincent <contact@cyrilvincent.com>
2020-05-28 09:49:28 +0200

    Skema 1.25/3

--- tppandasheart.py
+++ tppandasheart.py
@@ -1,5 +1,5 @@
 # Charger le fichier data/heartdisease/data_with_nan.csv pd.read_csv(filename,na_values=".")
-# Afficher l'entête du dataframe et noter les valeurs de l'avant dernière colonne
+# Afficher dataframe et noter les valeurs de l'avant dernière colonne
 # Effacer les colonnes slope,ca,thal : dataframe.drop(nomcol,1)
 # Nettoyer le fichier en remplacant les nan par 0 : fillna
 # Calculer la moyenne et l'écart type de chol
@@ -8,3 +8,13 @@
 
 import pandas as pd
 import numpy as np
+
+dataset = pd.read_csv("data/heartdisease/data_with_nan.csv", na_values=".")
+dataset = dataset.drop('slope',1)
+dataset = dataset.drop('ca',1)
+dataset = dataset.drop('thal',1)
+dataset2 = dataset.fillna(0)
+print(dataset2)
+print(np.mean(dataset2.chol), np.mean(dataset2.age))
+dataset3 = dataset.dropna()
+print(np.mean(dataset3.chol), np.mean(dataset3.age))
