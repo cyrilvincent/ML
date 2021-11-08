cf57d3d86bbd003e9ddd5a358e5d2068ae66d41a
cyrilvincent <contact@cyrilvincent.com>
2021-11-08 09:02:33 +0100

    ATP

--- 13-5-tp-sklearn-polynomial-todo.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import sklearn.linear_model as sklm
-
-data = pd.read_csv('data/house/house.csv')
-model = sklm.LinearRegression()
-x = data["surface"].values.reshape(-1,1)
-y = data["loyer"]
-
