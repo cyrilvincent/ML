2ca7a2a96c83653f83a027c9f6a02afbe5752fa3
cyrilvincent <contact@cyrilvincent.com>
2021-11-09 09:51:08 +0100

    ATP

--- demo_sklearn.py
+++ demo_sklearn.py
@@ -5,8 +5,12 @@ import sklearn.pipeline as pipe
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import sklearn.model_selection as ms
+
 
 print(sklearn.__version__)
+
+np.random.seed(0)
 
 dataframe = pd.read_csv("data/house/house.csv")
 dataframe = dataframe[dataframe.surface < 200]
@@ -14,12 +18,15 @@ dataframe = dataframe[dataframe.surface < 200]
 x = dataframe.surface.values.reshape(-1, 1)
 y = dataframe.loyer
 
-# model = lm.LinearRegression()
-for i in range(8):
+xtrain, xtest, ytrain, ytest = ms.train_test_split(x,y,train_size=0.8,test_size=0.2)
+
+model = lm.LinearRegression()
+for i in range(5):
     model = pipe.make_pipeline(pp.PolynomialFeatures(i), lm.Ridge(0))
-    model.fit(x, y)
+    model.fit(xtrain, ytrain)
     # print(model.coef_, model.intercept_)
-    print(i, model.score(x, y))
+    print(i, model.score(xtrain, ytrain))
+    print(i, model.score(xtest, ytest))
 
 
 # f = lambda x: model.coef_ * x + model.intercept_
