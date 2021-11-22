4ff7f257b85392a05d46f522a6c707c8ecf649ed
cyrilvincent <contact@cyrilvincent.com>
2021-11-22 15:41:26 +0100

    ESRF

--- demo_sklearn.py
+++ demo_sklearn.py
@@ -4,6 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sklearn.preprocessing as pp
 import sklearn.pipeline as pipe
+import sklearn.model_selection as ms
+import numpy as np
+
+np.random.seed(0)
 
 dataframe = pd.read_csv("data/house/house.csv")
 dataframe = dataframe[dataframe.surface < 200]
@@ -17,18 +21,23 @@ print(mat1)
 x = dataframe.surface.values.reshape(-1, 1)
 y = dataframe.loyer
 
+xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, train_size=0.8, test_size=0.2)
+
+
 # Instanciation du model
 # model = lm.LinearRegression()
 
 model = pipe.make_pipeline(pp.PolynomialFeatures(3), lm.Ridge())
 # Apprentissage
-model.fit(x, y)
+model.fit(xtrain, ytrain)
 # print(model.coef_, model.intercept_)
 # Prediction
 predicted = model.predict(np.arange(200).reshape(-1, 1))
 # Score
-score = model.score(x, y)
+score = model.score(xtest, ytest)
 print(score)
+# score = model.score(xtrain, ytrain)
+# print(score)
 
 # f = lambda x: model.coef_ * x + model.intercept_
 
