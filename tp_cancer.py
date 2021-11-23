1fa38dffb3ba4c2a86dd8eb5b99b3c6d7c44f9fa
cyrilvincent <contact@cyrilvincent.com>
2021-11-23 10:11:25 +0100

    ESRF

--- tp_cancer.py
+++ tp_cancer.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import sklearn.model_selection as ms
 import sklearn.neighbors as nn
+import sklearn.ensemble as rf
+import matplotlib.pyplot as plt
+import pickle
 
 dataframe = pd.read_csv("data/breast-cancer/data.csv", index_col='id')
 print(dataframe)
@@ -14,9 +17,18 @@ x = dataframe.drop("diagnosis", 1)
 
 np.random.seed(0)
 xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, train_size=0.8, test_size=0.2)
-model = nn.KNeighborsClassifier(n_neighbors=7)
+# model = nn.KNeighborsClassifier(n_neighbors=7)
+model = rf.RandomForestClassifier(warm_start=True)
 model.fit(xtrain, ytrain)
-print(model.score(xtest, ytest))
+score = model.score(xtest, ytest)
+
+with open(f"data/breast-cancer/rf_model_{score:.3f}.pickle", "wb") as f:
+    pickle.dump(model, f)
+
+model = None
+
+with open(f"data/breast-cancer/rf_model_0.974.pickle", "rb") as f:
+    model = pickle.load(f)
 
 ypredict = model.predict(xtest)
 errors = ypredict - ytest
@@ -25,5 +37,9 @@ score = 1 - len(errors[np.abs(errors) == 1]) / len(ytest)
 print(score)
 fp = 1 - len(errors[errors == 1]) / len(ytest)
 print(fp)
+
+plt.bar(xtest.columns, model.feature_importances_)
+plt.xticks(rotation = 90)
+plt.show()
 
 
