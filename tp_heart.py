dcd316a73a15f00b57ad56cda7feb036ef320709
cyrilvincent <contact@cyrilvincent.com>
2022-05-09 14:38:36 +0200

    ATP

---
+++ tp_heart.py
@@ -0,0 +1,4 @@
+# Dans heartdisease data_with_nan.csv & data_cleaned_up.csv
+# Le contenu du fichier est expliqué dans index.txt
+# Essayer de comprendre le fichier
+# Essayer de faire les stats appropriées sur data_cleaned_up.csv
