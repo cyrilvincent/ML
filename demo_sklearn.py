03b36357f7e054c7e566fe3cac5bb9131cde9f50
cyrilvincent <contact@cyrilvincent.com>
2022-05-10 10:10:08 +0200

    ATP

--- demo_sklearn.py
+++ demo_sklearn.py
@@ -6,17 +6,23 @@ import sklearn.preprocessing as pp
 import sklearn.pipeline as pipe
 import sklearn.linear_model as lm
 import numpy as np
+import sklearn.model_selection as ms
+
 
 print(sklearn.__version__)
+np.random.seed(0)
 
 dataframe = pd.read_csv("data/house/house.csv")
 
 y = dataframe.loyer
 x = dataframe.surface.values.reshape(-1, 1)
 
+xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, train_size=0.8, test_size=0.2)
+
 # model = LinearRegression()
-model = pipe.make_pipeline(pp.PolynomialFeatures(7), lm.Ridge())
-model.fit(x, y)
+model = pipe.make_pipeline(pp.PolynomialFeatures(2), lm.Ridge())
+model.fit(xtrain, ytrain)
+# Interdiction de fit sur xtest et ytest
 
 # print(model.coef_, model.intercept_)
 #
@@ -25,7 +31,9 @@ model.fit(x, y)
 
 y_pred = model.predict(np.arange(400).reshape(-1, 1))
 
-score = model.score(x, y)
+score = model.score(xtrain, ytrain)
+print(score)
+score = model.score(xtest, ytest)
 print(score)
 
 plt.scatter(x, y)
