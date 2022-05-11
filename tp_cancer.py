36adb3ad33262d09ec28a00c303f84756dba6d29
cyrilvincent <contact@cyrilvincent.com>
2022-05-11 10:02:57 +0200

    ATP

--- tp_cancer.py
+++ tp_cancer.py
@@ -7,6 +7,7 @@ from sklearn.tree import export_graphviz
 import matplotlib.pyplot as plt
 import pickle
 import sklearn.svm as svm
+import sklearn.neural_network as nn
 
 np.random.seed(0)
 dataframe = pd.read_csv("data/breast-cancer/data.csv")
@@ -15,8 +16,9 @@ x = dataframe.drop(["diagnosis", "id"], axis=1)
 
 xtrain, xtest, ytrain, ytest = ms.train_test_split(x,y, train_size=0.8, test_size=0.2)
 # model = ne.KNeighborsClassifier(n_neighbors=5)
-model = rf.RandomForestClassifier(warm_start=True)
+# model = rf.RandomForestClassifier(warm_start=True)
 # model = svm.SVC(kernel="linear")
+model = nn.MLPClassifier(hidden_layer_sizes=(30,20,10) )
 
 model.fit(xtrain, ytrain)
 score = model.score(xtest, ytest)
@@ -32,14 +34,14 @@ with open("data/breast-cancer/model_rf.pickle", "rb") as f:
 
 model.fit(x, y)
 
-print(model.feature_importances_)
-plt.bar(x.columns, model.feature_importances_)
-plt.xticks(rotation=45)
-plt.show()
-
-export_graphviz(model.estimators_[0],
-                out_file='data/breast-cancer/tree.dot',
-                feature_names = x.columns,
-                class_names = str(y),
-                rounded = True, proportion = False,
-                precision = 2, filled = True)
+# print(model.feature_importances_)
+# plt.bar(x.columns, model.feature_importances_)
+# plt.xticks(rotation=45)
+# plt.show()
+#
+# export_graphviz(model.estimators_[0],
+#                 out_file='data/breast-cancer/tree.dot',
+#                 feature_names = x.columns,
+#                 class_names = str(y),
+#                 rounded = True, proportion = False,
+#                 precision = 2, filled = True)
