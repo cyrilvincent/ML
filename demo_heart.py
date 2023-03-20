6d0d19d9765731732ea69ef95ef9c094cf83052c
Cyril Vincent <contact@cyrilvincent.com>
2023-03-20 15:20:33 +0100

    CEA

--- demo_heart.py
+++ demo_heart.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import sklearn.linear_model as lm
 
 df = pd.read_csv("data/heartdisease/data_cleaned_up.csv")
 print(df)
@@ -9,3 +10,11 @@ ko_set = training_set[df.num == 1]
 print(ok_set.describe().T)
 print(ko_set.describe().T)
 print(df.corr())
+
+model = lm.LinearRegression()
+model.fit(training_set, df.num)
+predicted = model.predict(training_set)
+print(model.score(training_set, df.num))
+
+print(model.coef_, model.intercept_)
+
