475b456504b39509c79e63d864ed24413b25d875
Cyril Vincent <contact@cyrilvincent.com>
2023-03-20 15:53:27 +0100

    CEA

--- demo_sklearn.py
+++ demo_sklearn.py
@@ -1,19 +1,29 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import sklearn.linear_model as lm
+import sklearn.pipeline as pipe
+import sklearn.preprocessing as pp
+import sklearn.model_selection as ms
+import numpy as np
 
 df = pd.read_csv("data/house/house.csv")
 df = df[df.surface < 175]
 print(df)
 
-model = lm.LinearRegression()
-model.fit(df.surface.values.reshape(-1, 1), df.loyer)
+np.random.seed(0)
+xtrain, xtest, ytrain, ytest = ms.train_test_split(df.surface.values.reshape(-1, 1), df.loyer)
 
-loyer_predict = model.predict(df.surface.values.reshape(-1, 1))
+# model = lm.LinearRegression()
+model = pipe.make_pipeline(pp.PolynomialFeatures(2), lm.Ridge())
 
-print(model.score(df.surface.values.reshape(-1, 1), df.loyer))
-print(model.coef_, model.intercept_)
+model.fit(xtrain.reshape(-1, 1), ytrain)
+# /!\ JAMAIS fit sur testset
+
+loyer_predict = model.predict(xtest.reshape(-1, 1))
+
+print(model.score(xtest.reshape(-1, 1), ytest))
+# print(model.coef_, model.intercept_)
 
 plt.scatter(df.surface, df.loyer)
-plt.plot(df.surface, loyer_predict, color="red")
+plt.scatter(xtest, loyer_predict, color="red")
 plt.show()
