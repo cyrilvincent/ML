6e52b71a17f40f155603e68d7ae834d0cd568ab5
Cyril Vincent <contact@cyrilvincent.com>
2023-03-21 09:33:31 +0100

    CEA

--- demo_cancer.py
+++ demo_cancer.py
@@ -1,11 +1,17 @@
 import pandas as pd
+import numpy as np
+import sklearn.model_selection as ms
+import sklearn.neighbors as nn
 
 df = pd.read_csv("data/breast-cancer/data.csv", index_col="id")
-print(df)
-target = df.diagnosis
-training_set = df.drop(["diagnosis"], 1)
-ok_set = training_set[df.diagnosis == 0]
-ko_set = training_set[df.diagnosis == 1]
-print(ok_set.describe().T)
-print(ko_set.describe().T)
-print(df.corr().T)
+np.random.seed(0)
+y = df.diagnosis
+x = df.drop("diagnosis", 1)
+
+xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, train_size=0.8, test_size=0.2)
+model = nn.KNeighborsClassifier(n_neighbors=5)
+model.fit(xtrain, ytrain)
+score = model.score(xtest, ytest)
+print(score)
+ypred = model.predict(xtest)
+print((ypred - ytest).values)
