72efc4da7630572ed4722ffd12c3de1f9926a9b8
Cyril Vincent <contact@cyrilvincent.com>
2023-11-06 11:24:28 +0100

    CEA

--- hello.py
+++ hello.py
@@ -1,12 +1,4 @@
-import matplotlib
-import matplotlib.pyplot as plt
 import numpy as np
 
 print("Hello world")
-print(matplotlib.__version__)
-
-x = np.arange(0, 1000)
-y = np.sin(x / 200)
-
-plt.plot(x, y)
-plt.show()
+print(np.__version__)
