2b173a17c322b87814437ea5bc1d0593766ffa2f
Cyril Vincent <contact@cyrilvincent.com>
2023-11-06 16:31:02 +0100

    CEA

--- demo_cancer.py
+++ demo_cancer.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import sklearn.linear_model as lm
+import sklearn.neighbors as nn
 
 dataframe = pd.read_csv("data/breast-cancer/data.csv", index_col="id")
 y = dataframe.diagnosis
 x = dataframe.drop("diagnosis", axis=1)
-model = lm.LinearRegression()
+# model = lm.LinearRegression()
+model = nn.KNeighborsClassifier(n_neighbors=3)
 model.fit(x, y)
-print(model.coef_, model.intercept_)
 print(model.score(x, y))
-print(dataframe)
