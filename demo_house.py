933aa88c1aa9117852db19baf53ce9a8b3006d77
cyrilvincent <contact@cyrilvincent.com>
2024-06-10 14:11:54 +0200

    CEA

--- demo_house.py
+++ demo_house.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import sklearn.linear_model as lm
 import sklearn.pipeline as pipe
 import sklearn.preprocessing as pp
+import numpy as np
 
 dataframe = pd.read_csv("data/house/house.csv")
 dataframe = dataframe[dataframe.surface < 200]
@@ -10,12 +11,12 @@ y = dataframe.loyer
 x = dataframe.surface.values.reshape(-1, 1)
 
 # model = lm.LinearRegression()
-model = pipe.make_pipeline(pp.PolynomialFeatures(1), lm.Ridge)
+model = pipe.make_pipeline(pp.PolynomialFeatures(10), lm.Ridge())
 model.fit(x, y)
 ypred = model.predict(x)
 score = model.score(x, y)
 print(score)
 
 plt.scatter(dataframe.surface, dataframe.loyer)
-plt.plot(x, ypred, color="red")
+plt.scatter(x, ypred, color="red")
 plt.show()
