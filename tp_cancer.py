35ec1bb444bca18e5286f72a4985d71c368484ba
cyrilvincent <contact@cyrilvincent.com>
2024-06-11 09:23:17 +0200

    CEA

--- tp_cancer.py
+++ tp_cancer.py
@@ -2,6 +2,8 @@ import pandas as pd
 import sklearn.neighbors as neighbors
 import sklearn.model_selection as ms
 import numpy as np
+import sklearn.ensemble as rf
+import matplotlib.pyplot as plt
 
 dataframe = pd.read_csv("data/breast-cancer/data.csv", index_col="id")
 print(dataframe)
@@ -12,10 +14,17 @@ np.random.seed(42)
 xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, train_size=0.8, test_size=0.2)
 print(xtest.shape, ytest.shape)
 
-for k in range(1, 15, 2):
-    model = neighbors.KNeighborsClassifier(n_neighbors=k)
-    model.fit(xtrain, ytrain)
-    print(model.score(xtrain, ytrain), model.score(xtest, ytest))
-
+model = rf.RandomForestClassifier()
+model.fit(xtrain, ytrain)
+# for k in range(1, 15, 2):
+#     model = neighbors.KNeighborsClassifier(n_neighbors=k)
+#     model.fit(xtrain, ytrain)
+#     print(model.score(xtrain, ytrain), model.score(xtest, ytest))
+print(model.score(xtrain, ytrain), model.score(xtest, ytest))
 ypred = model.predict(xtest)
 print(ypred)
+print(model.feature_importances_)
+
+plt.bar(x.columns, model.feature_importances_)
+plt.xticks(rotation=45)
+plt.show()
