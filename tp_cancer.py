8eba2f1d87643f7acc67f807ced17106baa5c704
cyrilvincent <contact@cyrilvincent.com>
2024-10-02 09:33:23 +0200

    CEA

--- tp_cancer.py
+++ tp_cancer.py
@@ -7,6 +7,7 @@ import pandas as pd
 import numpy as np
 import sklearn.neighbors as nn
 import sklearn.model_selection as ms
+import sklearn.preprocessing as pp
 
 dataframe = pd.read_csv("data/breast-cancer/data.csv", index_col="id")
 print(dataframe.describe())
@@ -17,6 +18,11 @@ y = dataframe["diagnosis"]
 x = dataframe.drop("diagnosis", axis=1)
 
 xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, train_size=0.8, test_size=0.2)
+
+scaler = pp.RobustScaler()
+scaler.fit(xtrain)
+xtrain = scaler.transform(xtrain)
+xtest = scaler.transform(xtest)
 
 for k in range(3,12,2):
     model = nn.KNeighborsClassifier(n_neighbors=k)
