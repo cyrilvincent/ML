24c8f9f0df00074f170d6adb47b6428d8358e930
cyrilvincent <contact@cyrilvincent.com>
2024-11-13 14:30:30 +0100

    CEA

---
+++ tp_heart.py
@@ -0,0 +1,11 @@
+# Pandas charger data/heartdisease/data_cleaned_up.csv
+# Analyse statistique
+# ok = num == 0
+# ko = num == 1
+# Faire des stats => describe()
+# y = colonne num
+# x toutes les autres colonnes : dataframe.drop("num", axis=1)
+# Créer le modèle LinearRegression
+# Fit
+# Predict
+# Score => non fonctionnel
