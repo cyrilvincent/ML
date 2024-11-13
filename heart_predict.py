7f7cb9192f2ed1ce69cf676855bfc1b4aaa129ca
cyrilvincent <contact@cyrilvincent.com>
2024-11-13 08:35:07 +0100

    CEA

--- heart_predict.py
+++
@@ -1,8 +0,0 @@
-import pickle
-import numpy as np
-
-with open("data/heartdisease/rf.pickle", "rb") as f:
-    model = pickle.load(f)
-    data = np.array([[28,1,2,130,132,0,2,185,0,0]])
-    ypred = model.predict(data)
-    print(ypred[0])
--- hello.py
+++
@@ -1,6 +0,0 @@
-import sys
-import matplotlib
-import numpy
-print("Hello")
-print(sys.version)
-print(matplotlib.__version__)
