505b2c16850f00f9baf0e771636911216f233442
cyrilvincent <contact@cyrilvincent.com>
2024-11-14 13:43:41 +0100

    CEA

--- tp_cancer.py
+++ tp_cancer.py
@@ -10,11 +10,22 @@ y = dataframe.diagnosis
 x = dataframe.drop("diagnosis", axis=1)
 
 np.random.seed(0)
-xtrain, xtest, ytrain, ytest = ms.train_test_split(train_size=0.8, test_size=0.2)
+xtrain, xtest, ytrain, ytest = ms.train_test_split(x,y,train_size=0.8, test_size=0.2)
 
 scaler = pp.RobustScaler()
 scaler.fit(xtrain)
 xtrain = scaler.transform(xtrain)
 xtest = scaler.transform(xtest)
 
+model = nn.KNeighborsClassifier(n_neighbors=3)
+model.fit(xtrain, ytrain)
+predicted = model.predict(xtest)
+print(model.score(xtest, ytest))
+
+plt.bar
+
+xsimple = np.array([[17.99,10.38,122.8,1001,0.1184,0.2776,0.3001,0.1471,0.2419,0.07871,1.095,0.9053,8.589,153.4,0.006399,0.04904,0.05373,0.01587,0.03003,0.006193,25.38,17.33,184.6,2019,0.1622,0.6656,0.7119,0.2654,0.4601,0.1189]])
+xsimple = scaler.transform(xsimple)
+predicted = model.predict(xsimple)
+print(predicted)
 
--- tp_heart.py
+++ tp_heart.py
@@ -20,6 +20,7 @@ import numpy as np
 import sklearn.preprocessing as pp
 import sklearn.pipeline as pipe
 import sklearn.model_selection as ms
+import sklearn.ensemble as rf
 
 
 dataframe = pd.read_csv("data/heartdisease/data_cleaned_up.csv")
@@ -28,20 +29,36 @@ ko = dataframe[dataframe.num == 1]
 
 y = dataframe.num
 x = dataframe.drop("num", axis=1)
+x["random"] = np.random.rand(len(x))
 
 np.random.seed(42)
 xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, train_size=0.8, test_size=0.2)
 
 # model = lm.LinearRegression()
-model = pipe.make_pipeline(pp.PolynomialFeatures(3), lm.Ridge())
+# model = pipe.make_pipeline(pp.PolynomialFeatures(3), lm.Ridge())
+model = rf.RandomForestClassifier()
 model.fit(xtrain, ytrain)
 
 predicted = model.predict(xtest)
 score1 = model.score(xtest, ytest)
 score2 = model.score(xtrain, ytrain)
 
+print(model.feature_importances_)
+
+plt.bar(x.columns, model.feature_importances_)
+plt.xticks(rotation=45)
+plt.show()
+
 print(ok.describe())
 print(ko.describe())
 print(score1, score2)
 
 # print(np.abs(dataframe.corr()))
+
+from sklearn.tree import export_graphviz
+
+export_graphviz(model.estimators_[0],
+                out_file="data/heartdisease/tree.dot",
+                feature_names=x.columns,
+                class_names=["0", "1"],
+                )
