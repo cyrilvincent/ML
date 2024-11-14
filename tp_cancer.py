1081f7427e2a69281e7287d516648a7d49d486c1
cyrilvincent <contact@cyrilvincent.com>
2024-11-14 15:27:45 +0100

    CEA

--- tp_cancer.py
+++ tp_cancer.py
@@ -6,6 +6,7 @@ import sklearn.neighbors as nn
 import sklearn.preprocessing as pp
 import sklearn.ensemble as rf
 import matplotlib.pyplot as plt
+import pickle
 
 dataframe = pd.read_csv("data/breast-cancer/data.csv", index_col="id")
 y = dataframe.diagnosis
@@ -24,11 +25,18 @@ xtest = scaler.transform(xtest)
 model = rf.RandomForestClassifier()
 model.fit(xtrain, ytrain)
 predicted = model.predict(xtest)
-print(model.score(xtest, ytest))
+score = model.score(xtest, ytest)
+print(score)
+
+with open(f"data/breast-cancer/rf-{score*100:.2f}.pickle", "wb") as f:
+    pickle.dump((scaler, model), f)
 
 plt.bar(x.columns, model.feature_importances_)
 plt.xticks(rotation=45)
 plt.show()
+
+# pickle.dump((x,y), f)
+# x, y = pickle.load(f)
 
 
 xsimple = np.array([[17.99,10.38,122.8,1001,0.1184,0.2776,0.3001,0.1471,0.2419,0.07871,1.095,0.9053,8.589,153.4,0.006399,0.04904,0.05373,0.01587,0.03003,0.006193,25.38,17.33,184.6,2019,0.1622,0.6656,0.7119,0.2654,0.4601,0.1189,0]])
