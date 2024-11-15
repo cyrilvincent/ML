dc3c66bfce165f61318928a63ea61cd9f81dd0ee
cyrilvincent <contact@cyrilvincent.com>
2024-11-15 11:58:17 +0100

    CEA

--- tp_cancer.py
+++ tp_cancer.py
@@ -7,13 +7,16 @@ import sklearn.preprocessing as pp
 import sklearn.ensemble as rf
 import matplotlib.pyplot as plt
 import pickle
+import sklearn.svm as svm
+import sklearn.neural_network as neural
 
 dataframe = pd.read_csv("data/breast-cancer/data.csv", index_col="id")
 y = dataframe.diagnosis
 x = dataframe.drop("diagnosis", axis=1)
+np.random.seed(0)
 x["random"] = np.random.rand(len(x))
 
-np.random.seed(0)
+
 xtrain, xtest, ytrain, ytest = ms.train_test_split(x,y,train_size=0.8, test_size=0.2)
 
 scaler = pp.RobustScaler()
@@ -22,7 +25,9 @@ xtrain = scaler.transform(xtrain)
 xtest = scaler.transform(xtest)
 
 #model = nn.KNeighborsClassifier(n_neighbors=3)
-model = rf.RandomForestClassifier()
+# model = rf.RandomForestClassifier()
+# model = svm.SVC(C=1.0, kernel="linear")
+model = neural.MLPClassifier(hidden_layer_sizes=(30,30,30,30,30,30,30,30))
 model.fit(xtrain, ytrain)
 predicted = model.predict(xtest)
 score = model.score(xtest, ytest)
@@ -31,9 +36,9 @@ print(score)
 with open(f"data/breast-cancer/rf-{score*100:.2f}.pickle", "wb") as f:
     pickle.dump((scaler, model), f)
 
-plt.bar(x.columns, model.feature_importances_)
-plt.xticks(rotation=45)
-plt.show()
+# plt.bar(x.columns, model.feature_importances_)
+# plt.xticks(rotation=45)
+# plt.show()
 
 # pickle.dump((x,y), f)
 # x, y = pickle.load(f)
@@ -44,11 +49,11 @@ xsimple = scaler.transform(xsimple)
 predicted = model.predict(xsimple)
 print(predicted)
 
-from sklearn.tree import export_graphviz
-
-export_graphviz(model.estimators_[0],
-                out_file="data/breast-cancer/tree.dot",
-                feature_names=x.columns,
-                class_names=["0", "1"],
-                )
+# from sklearn.tree import export_graphviz
+#
+# export_graphviz(model.estimators_[0],
+#                 out_file="data/breast-cancer/tree.dot",
+#                 feature_names=x.columns,
+#                 class_names=["0", "1"],
+#                 )
 
