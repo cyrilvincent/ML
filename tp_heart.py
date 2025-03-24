5684d7a312b75ff9c5476f0319e9f5a603169088
cyrilvincent <contact@cyrilvincent.com>
2025-03-24 16:05:44 +0100

    CEA

--- tp_heart.py
+++ tp_heart.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import sklearn.linear_model as lm
 import numpy as np
+import sklearn.neighbors as n
+import sklearn.model_selection as ms
 
 
 
@@ -9,14 +11,19 @@ dataframe = pd.read_csv("data/heartdisease/data_cleaned_up.csv")
 y = dataframe.num
 x = dataframe.drop("num", axis=1)
 
-model = lm.LinearRegression()
+xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, test_size=0.2, train_size=0.8)
 
-model.fit(x, y)
+# model = lm.LinearRegression()
+model = n.KNeighborsClassifier(n_neighbors=3)
 
-score = model.score(x, y)
+model.fit(xtrain, ytrain)
+
+score = model.score(xtrain, ytrain)
+print(score)
+score = model.score(xtest, ytest)
 print(score)
 
-predicted = model.predict(x)
+predicted = model.predict(xtest)
 print(predicted)
 
 # LinearModel
