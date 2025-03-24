5867812b2d8677de1e4d25fe86746fe882f01ceb
cyrilvincent <contact@cyrilvincent.com>
2025-03-24 14:30:50 +0100

    CEA

--- tp_heart.py
+++ tp_heart.py
@@ -6,6 +6,16 @@ dataframe = pd.read_csv("data/heartdisease/data_cleaned_up.csv")
 y = dataframe.num
 x = dataframe.drop("num", axis=1)
 
+model = lm.LinearRegression()
+
+model.fit(x, y)
+
+score = model.score(x, y)
+print(score)
+
+predicted = model.predict(x)
+print(predicted)
+
 # LinearModel
 # Fit
 # Predict
