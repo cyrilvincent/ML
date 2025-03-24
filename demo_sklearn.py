6b526e64d8bca4c103600ec69ec95d75ae48b1a3
cyrilvincent <contact@cyrilvincent.com>
2025-03-24 14:41:31 +0100

    CEA

--- demo_sklearn.py
+++ demo_sklearn.py
@@ -2,6 +2,9 @@ import sklearn
 import sklearn.linear_model as lm
 import pandas as pd
 import matplotlib.pyplot as plt
+import sklearn.preprocessing as pp
+import sklearn.pipeline as pipe
+import numpy as np
 
 print(sklearn.__version__)
 
@@ -14,7 +17,8 @@ x = dataframe.surface.values.reshape(-1,1)
 y = dataframe.loyer
 
 # Step 2 : Instanciation du modèle : choix et création du modèle
-model = lm.LinearRegression()
+# model = lm.LinearRegression()
+model = pipe.make_pipeline(pp.PolynomialFeatures(2), lm.Ridge())
 
 # Step 3 : Apprentissage
 model.fit(x, y)
@@ -24,8 +28,9 @@ score = model.score(x, y) # (ypredicted - y) ** 2 avec un min_max_scaler à 0..1
 print(f"Score: {score:.2f}")
 
 # Step 5 : Predict
-ypredicted = model.predict(x)
+xtest = np.arange(400)
+ypredicted = model.predict(xtest.reshape(-1,1))
 
-plt.plot(dataframe.surface, ypredicted, color="red")
+plt.plot(xtest, ypredicted, color="red")
 plt.show()
 
