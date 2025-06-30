a2edf372c6a1f78fc8064280d7b8aeb2d5e2fb3e
Cyril Vincent <contact@cyrilvincent.com>
2025-06-30 18:42:56 +0200

    SQLI

--- heart_predict.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import numpy as np
-
-with open(f"data/heartdisease/rf-100.pickle", "rb") as f:
-    (model, scaler) = pickle.load(f)
-
-x = np.array([[28,1,2,130,132,0,2,185,0,0,0]])
-ypredicted = model.predict(x)
-print(ypredicted[0])
