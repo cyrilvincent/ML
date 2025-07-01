535fae527b1f17feedeb1712966acb7ab831e684
Cyril Vincent <contact@cyrilvincent.com>
2025-07-01 11:36:37 +0200

    SQLI

--- tp_retraite.py
+++ tp_retraite.py
@@ -5,3 +5,17 @@
 # nb_trimestre = input
 # 64 + (172 - nb_trimestre) / 4
 
+age = input("Age: ")
+age = int(age)
+if age > 67:
+    print("Taux plein")
+elif age < 64:
+    print("Pas de retraite")
+else:
+    nb_trimestre = int(input("Nb trimestre: "))
+    age_retraite = 64 + (172 - nb_trimestre) / 4
+    if age_retraite > 67:
+        age_retraite = 67
+    print(f"Age de la retraite à taux plein: {age_retraite:.1f}")
+
+
