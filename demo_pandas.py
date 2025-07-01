1f3140f7fefe763cdfbf7a1464515c3ac7492534
Cyril Vincent <contact@cyrilvincent.com>
2025-07-01 15:57:21 +0200

    SQLI

---
+++ demo_pandas.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+print(pd.__version__)
+
+dataframe = pd.read_csv("data/house/house.csv")
+print(dataframe)
+print(dataframe.describe())
+dataframe["loyer_m2"] = dataframe.loyer / dataframe["surface"]
+dataframe.to_excel("data/house/house.xlsx", index=False)  # openpyxl
+
+
+
+
+
