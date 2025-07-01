34ac5ef43b55e2dde4bfd62af0d4da7995f71009
Cyril Vincent <contact@cyrilvincent.com>
2025-07-01 13:54:31 +0200

    SQLI

---
+++ tp_boucle.py
@@ -0,0 +1,4 @@
+# 1 Afficher les nombres pairs < 100
+# 2 Saisir un chiffre et afficher sa factorielle, ex 5! = 1*2*3*4*5 = 120
+# 3 Bonus : Saisir un chiffre et afficher s'il est premier ou non, tout nombre n >= 2 est premier sauf s'il possède un diviseur entre 2 et n-1
+# Rappel 8 % 2 == 0
