469da0a0fd2c0398f51d8b63c13c818f75e9331d
Cyril Vincent <contact@cyrilvincent.com>
2025-07-03 10:32:54 +0200

    SQLI

--- tp_cancer.py
+++ tp_cancer.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import sklearn.preprocessing as pp
 
 # Charger data/breast-cancer/data.csv dans un dataset
 # Faire un describe sur la colonne perimeter_worst
@@ -8,6 +10,38 @@ import matplotlib.pyplot as plt
 dataframe = pd.read_csv("data/breast-cancer/data.csv")
 print(dataframe["perimeter_worst"].describe())
 dataframe.to_html("data/breast-cancer/data.html")
-plt.matshow(dataframe.corr())
-plt.show()
+# plt.matshow(dataframe.corr())
+# plt.show()
+
+
+
+y = dataframe.diagnosis
+x = dataframe.drop(["diagnosis", "id"], axis=1)
+
+# train_test_split
+
+scaler = pp.StandardScaler()
+scaler.fit(x, y)
+xnorm = scaler.transform(x)
+print(xnorm)
+
+
+
+
+
+# print(x["perimeter_mean"])
+# mean = np.mean(x["perimeter_mean"])
+# std = np.std(x["perimeter_mean"])
+# print(f"Mean: {mean}, std:{std}")
+# norm = (x["perimeter_mean"] - mean) / std
+# print(norm)
+#
+# print(x["smoothness_se"])
+# mean = np.mean(x["smoothness_se"])
+# std = np.std(x["smoothness_se"])
+# print(f"Mean: {mean}, std:{std}")
+# norm = (x["smoothness_se"] - mean) / std
+# print(norm)
+
+
 
--- tp_mnist.py
+++ tp_mnist.py
@@ -13,7 +13,13 @@ print(xtrain.shape, ytrain.shape, xtest.shape, ytest.shape)
 xtrain = xtrain.reshape(-1, 28*28) # 764
 xtest = xtest.reshape(-1, 28*28)
 
-# TODO
+for k in range(3,12):
+    model = nn.KNeighborsClassifier(n_neighbors=k)
+    model.fit(xtrain, ytrain)
+    ypredicted = model.predict(xtest)
+    score = model.score(xtest, ytest)
+    print(f"K={k} Score: {score:.3f}")
+
 # KNN
 # FIT
 # PREDICT => ypredicted
@@ -28,20 +34,20 @@ for index, value in enumerate(select):
     plt.subplot(3, 4, index + 1)
     plt.axis("off")
     plt.imshow(xtest[value], cmap=plt.cm.gray_r, interpolation="nearest")
-    # plt.title(f"Predicted {ypredicted[value]}")
+    plt.title(f"Predicted {ypredicted[value]}")
 plt.show()
 
-# errors = ytest != ypredicted
-# xerrors = xtest[errors]
-# yerrors = ypredicted[errors]
-#
-# select = np.random.randint(xerrors.shape[0], size=12)
-#
-# for index, value in enumerate(select):
-#     plt.subplot(3, 4, index + 1)
-#     plt.axis("off")
-#     plt.imshow(xerrors[value], cmap=plt.cm.gray_r, interpolation="nearest")
-#     plt.title(f"Predicted {yerrors[value]}")
-# plt.show()
+errors = ytest != ypredicted
+xerrors = xtest[errors]
+yerrors = ypredicted[errors]
+
+select = np.random.randint(xerrors.shape[0], size=12)
+
+for index, value in enumerate(select):
+    plt.subplot(3, 4, index + 1)
+    plt.axis("off")
+    plt.imshow(xerrors[value], cmap=plt.cm.gray_r, interpolation="nearest")
+    plt.title(f"Predicted {yerrors[value]}")
+plt.show()
 
 
