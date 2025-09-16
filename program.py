80491e891f70ad85a8a2623274353996980ac8af
Cyril Vincent <contact@cyrilvincent.com>
2025-09-16 14:18:55 +0200

    Sparks

--- program.py
+++
@@ -1,9 +0,0 @@
-# import demo_function
-#
-# result = demo_function.add(2,3)
-
-from demo_function import add
-from tp_function import factorielle, is_prime
-
-print(factorielle(5))
-print(is_prime(7))
