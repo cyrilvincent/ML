5f07530800f40869069dc677f7f99619fdea7294
Cyril Vincent <contact@cyrilvincent.com>
2025-09-18 10:53:45 +0200

    Sparks

--- tp_heart.py
+++ tp_heart.py
@@ -1,2 +1,16 @@
 # Charger le fichier data_cleaned_up.csv avec pandas
 # Faire un describe
+
+import numpy as np
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+dataframe=pd.read_csv("data/heartdisease/data_cleaned_up.csv")
+print(dataframe.describe().T)
+
+# ok = num==0
+# ko = num==1
+# Faire un describe sur les 3 dataframes => en tirer des hypothèses
+
+
--- tp_pandas.py
+++ tp_pandas.py
@@ -2,3 +2,31 @@
 # Faire un describe
 # Créer la colonne loyer_m2
 # Sauvegarder dans le format de votre choix
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import xlrd
+import sqlite3
+
+dataframe = pd.read_csv("data/house/house.csv", delimiter=",")
+
+dataframe["surface"] = dataframe["surface"].astype(np.float64) # Changement de type
+dataframe["loyer_m2"] = dataframe["loyer"] / dataframe["surface"]
+# dataframe.to_excel("data/house/house.xlsx", index=False)
+dataframe.to_html("data/house/house.html", index=False)
+
+dataframe = dataframe[dataframe["surface"] < 200]
+plt.scatter(dataframe["surface"], dataframe["loyer"])
+
+print(dataframe.dtypes)
+print(dataframe)
+# print(dataframe.values) # convert to numpy
+print(dataframe.describe())
+plt.show()
+
+# Filtrer la surfaces < 200
+# Calculer la mean et la std des loyers
+# Enlever les points > mean + 3*std
+# Reafficher les données filtrées
+# Save
