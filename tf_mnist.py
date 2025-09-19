2eb1588a8705f349812c260147466809059fc371
Cyril Vincent <contact@cyrilvincent.com>
2025-09-19 15:47:40 +0200

    Sparks

--- tf_mnist.py
+++ tf_mnist.py
@@ -19,9 +19,9 @@ x_test = x_test.reshape(-1,28*28)
 y_train = tf.keras.utils.to_categorical(y_train)
 y_test = tf.keras.utils.to_categorical(y_test)
 
-sample = np.random.randint(60000, size=5000)
-x_train = x_train[sample]
-y_train = y_train[sample]
+# sample = np.random.randint(60000, size=5000)
+# x_train = x_train[sample]
+# y_train = y_train[sample]
 
 model = tf.keras.Sequential([
     tf.keras.layers.Dense(600, input_shape=(x_train.shape[1],)),
@@ -31,7 +31,7 @@ model = tf.keras.Sequential([
     tf.keras.layers.Dense(10, activation=tf.nn.softmax),
   ])
 model.compile(loss="categorical_crossentropy", metrics=['accuracy'])
-trained = model.fit(x_train, y_train, epochs=5, batch_size=10,validation_data=(x_test, y_test))
+trained = model.fit(x_train, y_train, epochs=50, batch_size=2,validation_data=(x_test, y_test))
 print(model.summary())
 
 predicted = model.predict(x_test)
