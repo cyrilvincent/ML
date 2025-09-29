9ae115eff1ccc5b5347d979123bc95d41ef11385
cyrilvincent <contact@cyrilvincent.com>
2025-09-29 15:39:47 +0200

    CEA

--- demo_sklearn.py
+++ demo_sklearn.py
@@ -3,8 +3,14 @@ import pandas as pd
 import sklearn.linear_model as lm
 import matplotlib.pyplot as plt
 import numpy as np
+import sklearn.pipeline as pipe
+import sklearn.preprocessing as pp
+import sklearn.model_selection as ms
+
 
 print(sklearn.__version__)
+# Set the seed
+np.random.seed(42)
 
 #1 Load data
 dataframe = pd.read_csv("data/house/house.csv")
@@ -13,18 +19,22 @@ dataframe = pd.read_csv("data/house/house.csv")
 y = dataframe["loyer"]
 x = dataframe["surface"].values.reshape(-1, 1)
 
-#3 & 4
+#3 Train Test Split
+xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, train_size=0.8, test_size=0.2)
 
 #5 Creating the model
-model = lm.LinearRegression()
+# model = lm.LinearRegression()
+model = pipe.make_pipeline(pp.PolynomialFeatures(2), lm.Ridge())
+
 # f(x) = ax + b => 2 poids
 
 #6 Fit
-model.fit(x, y)
+model.fit(xtrain, ytrain)
 
 #7 Scoring (facultatif)
-score = model.score(x, y)
-print(f"Score: {score:.2f}")
+training_score= model.score(xtrain, ytrain)
+testing_score = model.score(xtest, ytest)
+print(f"Training Score: {training_score:.2f} Testing Score: {testing_score:.2f}")
 
 #8 Predict
 ypredicted = model.predict(x)
--- tp_heart.py
+++ tp_heart.py
@@ -12,3 +12,14 @@ dataframe = pd.read_csv("data/heartdisease/data_cleaned_up.csv")
 #2 Make dataset
 y = dataframe["num"]
 x = dataframe.drop("num", axis=1)
+
+#5 Creating the model
+model = lm.LinearRegression()
+# f(x) = ax + b => 2 poids
+
+#6 Fit
+model.fit(x, y)
+
+#7 Scoring (facultatif)
+score = model.score(x, y)
+print(f"Score: {score:.2f}")
