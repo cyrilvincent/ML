fcbfe492954d2b0da5d9c3fccc3c85f4dab840a1
cyrilvincent <contact@cyrilvincent.com>
2025-09-30 10:35:26 +0200

    CEA

--- tp_heart.py
+++ tp_heart.py
@@ -6,6 +6,7 @@ import numpy as np
 import sklearn.neighbors as n
 import sklearn.model_selection as ms
 import sklearn.preprocessing as pp
+import sklearn.ensemble as rf
 
 print(sklearn.__version__)
 np.random.seed(42)
@@ -27,13 +28,27 @@ xtest = scaler.transform(xtest)
 
 #5 Creating the model
 # model = lm.LinearRegression()
-for k in range(3,12,2):
-    model = n.KNeighborsClassifier(n_neighbors=k)
-    # f(x) = ax + b => 2 poids
+# for k in range(3,12,2):
+#     model = n.KNeighborsClassifier(n_neighbors=k)
+#     # f(x) = ax + b => 2 poids
+#
+#     #6 Fit
+#     model.fit(xtrain, ytrain)
+#
+#     #7 Scoring (facultatif)
+#     score = model.score(xtest, ytest)
+#     print(f"Score: {k} {score:.2f}")
 
-    #6 Fit
-    model.fit(xtrain, ytrain)
+model = rf.RandomForestClassifier(max_depth=5)
+model.fit(xtrain, ytrain)
+test_score = model.score(xtest, ytest)
+train_score = model.score(xtrain, ytrain)
+print(train_score, test_score)
 
-    #7 Scoring (facultatif)
-    score = model.score(xtest, ytest)
-    print(f"Score: {k} {score:.2f}")
+from sklearn.tree import export_graphviz
+export_graphviz(model.estimators_[0], out_file="data/heartdisease/tree.dot", feature_names=list(x.columns), class_names=["0", "1"])
+
+print(model.feature_importances_)
+plt.bar(x.columns, model.feature_importances_)
+plt.xticks(rotation=45)
+plt.show()
