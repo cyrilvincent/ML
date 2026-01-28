ad50321f2e8ea11cac0df30572d8f7da83ab8c47
cyrilvincent <contact@cyrilvincent.com>
2026-01-28 10:25:04 +0100

    ATP

--- tp_heart.py
+++ tp_heart.py
@@ -1,9 +1,9 @@
-# 1 Refaire fonctionner house_regression.py
-
 import sklearn.linear_model as lm
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import sklearn.neighbors as n
+import sklearn.model_selection as ms
+np.random.seed(42)
 
 pd.set_option('display.max_columns', None)
 dataframe = pd.read_csv("data/heartdisease/data_cleaned_up.csv")
@@ -13,4 +13,11 @@ print(dataframe.describe())
 y = dataframe["num"]
 x = dataframe.drop(["num"], axis=1)
 
-print(dataframe.corr())
+xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, train_size=0.8, test_size=0.2,)
+
+model = n.KNeighborsClassifier(n_neighbors=3)
+
+model.fit(xtrain, ytrain)
+score_train = model.score(xtrain, ytrain)
+score_test = model.score(xtest, ytest)
+print(score_train, score_test)
