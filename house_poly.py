279bded863459f7f5ce3ff89d78b49280af1fc5d
cyrilvincent <contact@cyrilvincent.com>
2026-01-28 09:40:39 +0100

    ATP

---
+++ house_poly.py
@@ -0,0 +1,25 @@
+import sklearn.linear_model as lm
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn.preprocessing as pp
+import sklearn.pipeline as pipe
+
+dataframe = pd.read_csv("data/house/house.csv")
+
+x = dataframe["surface"].values.reshape(-1, 1)
+y = dataframe["loyer"]
+
+# model = lm.LinearRegression()
+for i in range(1,5):
+    model = pipe.make_pipeline(pp.PolynomialFeatures(i), lm.Ridge())
+    model.fit(x, y)
+
+    xnew = np.arange(400).reshape(-1, 1)
+    ypredicted = model.predict(xnew)
+    score = model.score(x, y)
+    print(f"Score: {score}")
+
+plt.scatter(dataframe["surface"], dataframe["loyer"])
+plt.plot(np.arange(400), ypredicted, color="red")
+plt.show()
--- house_regression.py
+++ house_regression.py
@@ -3,19 +3,26 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn.neural_network as nn
+import sklearn.model_selection as ms
+import numpy as np
 
+np.random.seed(42)
 dataframe = pd.read_csv("data/house/house.csv")
 
 x = dataframe["surface"].values.reshape(-1, 1)
 y = dataframe["loyer"]
 
+xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, train_size=0.8, test_size=0.2,)
+
 model = lm.LinearRegression()
-model.fit(x, y)
+model.fit(xtrain, ytrain)
 
 xnew = np.arange(400).reshape(-1, 1)
 ypredicted = model.predict(xnew)
-score = model.score(x, y)
-print(f"Score: {score}")
+
+score_train = model.score(xtrain, ytrain)
+score_test = model.score(xtest, ytest)
+print(f"Score: {score_train} {score_test}")
 
 plt.scatter(dataframe["surface"], dataframe["loyer"])
 plt.plot(np.arange(400), ypredicted, color="red")
