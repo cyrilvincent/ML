db6abfd3bf2038139ce0b851f4292c704d5ffead
cyrilvincent <contact@cyrilvincent.com>
2026-03-16 15:19:59 +0100

    CEA

--- tp_cancer.py
+++ tp_cancer.py
@@ -11,10 +11,20 @@ import numpy as np
 df = pd.read_csv("data/breast-cancer/data.csv")
 print(df.describe())
 
+np.random.seed(42)
+
 y = df["diagnosis"]
 x = df.drop(["diagnosis", "id"], axis=1)
 
-print(x.shape)
+xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, train_size=0.8, test_size=0.2)
 
-model = n.KNeighborsClassifier(n_neighbors=3)
+print(xtest.shape)
+
+for k in range(3, 15, 2):
+    model = n.KNeighborsClassifier(n_neighbors=k)
+    model.fit(xtrain, ytrain)
+    print(f"Train score for k={k}: {model.score(xtrain, ytrain):.2f}")
+    print(f"Test score for k={k}: {model.score(xtest, ytest):.2f}")
+
+
 
---
+++ tp_mnist.py
@@ -0,0 +1,55 @@
+import sklearn
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn.linear_model as lm
+import sklearn.pipeline as pipe
+import sklearn.preprocessing as pp
+import sklearn.model_selection as ms
+import sklearn.neighbors as n
+import numpy as np
+
+data = np.load("data/mnist/mnist.npz")
+print(data)
+xtrain = data["x_train"]
+xtest = data["x_test"]
+ytrain = data["y_train"]
+ytest = data["y_test"]
+
+print(xtest.shape)
+
+
+np.random.seed(42)
+
+xtrain = xtrain.reshape(-1, 28*28)
+xtest = xtest.reshape(-1, 28*28)
+print(xtest.shape)
+
+model = n.KNeighborsClassifier(n_neighbors=3)
+model.fit(xtrain, ytrain)
+print(f"Train score for k=3: {model.score(xtrain, ytrain):.2f}")
+print(f"Test score for k=3: {model.score(xtest, ytest):.2f}")
+
+ypred = model.predict(xtest)
+xtest = xtest.reshape(-1, 28, 28)
+select = np.random.randint(xtest.shape[0], size=12)
+
+for index, value in enumerate(select):
+    plt.subplot(3,4, index + 1)
+    plt.axis("off")
+    plt.imshow(xtest[value], cmap=plt.cm.gray_r)
+    plt.title(f"Predicted {ypred[value]}")
+plt.show()
+
+errors = ytest != ypred
+xerrors = xtest[errors]
+yerrors = ytest[errors]
+
+for index, value in enumerate(errors):
+    plt.subplot(3,4, index + 1)
+    plt.axis("off")
+    plt.imshow(xerrors[value], cmap=plt.cm.gray_r)
+    plt.title(f"Predicted {yerrors[value]}")
+plt.show()
+
+
+
