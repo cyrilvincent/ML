6c6ae73defbb8b6543317cb2c33d6b50f41651b0
Cyril Vincent <contact@cyrilvincent.com>
2026-03-23 16:32:23 +0100

    Orthotaxy

--- tp_mnist.py
+++ tp_mnist.py
@@ -25,7 +25,8 @@ xtrain = xtrain.reshape(-1, 28*28)
 xtest = xtest.reshape(-1, 28*28)
 print(xtest.shape)
 
-model = n.KNeighborsClassifier(n_neighbors=3)
+# model = n.KNeighborsClassifier(n_neighbors=3)
+model = rf.RandomForestClassifier()
 model.fit(xtrain, ytrain)
 print(f"Train score for k=3: {model.score(xtrain, ytrain):.2f}")
 print(f"Test score for k=3: {model.score(xtest, ytest):.2f}")
@@ -54,6 +55,8 @@ for index, value in enumerate(select):
     plt.title(f"Predicted {yerrors[value]}")
 plt.show()
 
+plt.imshow(model.feature_importances_.reshape(28,28))
+plt.show()
 
 
 
