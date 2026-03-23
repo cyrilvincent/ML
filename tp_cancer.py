ec77a0fafac7564953ed3da165d09c1021b238b1
Cyril Vincent <contact@cyrilvincent.com>
2026-03-23 14:46:53 +0100

    Orthotaxy

---
+++ tp_cancer.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import sklearn.linear_model as lm
+import matplotlib.pyplot as plt
+import sklearn.pipeline as pipe
+import sklearn.preprocessing as pre
+import sklearn.model_selection as ms
+import sklearn.neighbors as n
+import sklearn.preprocessing as pp
+import numpy as np
+
+df = pd.read_csv("data/breast-cancer/data.csv")
+
+y = df["diagnosis"]
+x = df.drop(["diagnosis", "id"], axis=1)
+
+np.random.seed(42)
+
+xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, train_size=0.8, test_size=0.2)
+
+scaler = pp.RobustScaler()
+scaler.fit(xtrain)
+xtrain = scaler.transform(xtrain)
+xtest = scaler.transform(xtest)
+
+model = n.KNeighborsClassifier(n_neighbors=3)
+model.fit(xtrain, ytrain)
+
+ypred = model.predict(xtest)
+
+trainscore = model.score(xtrain, ytrain)
+testscore = model.score(xtest, ytest)
+
+print(trainscore, testscore)
