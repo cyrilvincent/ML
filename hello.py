72786e9cec3d62b49e250d8c7cdc991b09ff1ad6
Cyril Vincent <contact@cyrilvincent.com>
2026-03-23 11:34:58 +0100

    Orthotaxy

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ import sklearn
 import pandas as pd
 # pip install openpyxl
 import numpy as np
+import matplotlib.pyplot as plt
 
 print("Hello")
 print(sklearn.__version__)
@@ -12,9 +13,18 @@ print(df)
 df.to_json("data/house/house.json", indent=2, index=False)
 df.to_excel("data/house/house.xlsx", index=False)
 
+print(df.describe())
+print(df.corr())
+
 v12 = np.arange(12, dtype=np.float64)
 print(v12)
 m34 = v12.reshape(3, 4)
 print(m34)
 print(m34 * 2)
 print(np.cos(m34))
+
+plt.scatter(df["surface"], df["loyer"])
+plt.show()
+
+df.hist()
+plt.show()
