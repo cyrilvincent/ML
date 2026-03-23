4fe48af6d7a42fe33d92370065105bbb94a63022
Cyril Vincent <contact@cyrilvincent.com>
2026-03-23 11:27:12 +0100

    Orthotaxy

---
+++ hello.py
@@ -0,0 +1,20 @@
+import sklearn
+import pandas as pd
+# pip install openpyxl
+import numpy as np
+
+print("Hello")
+print(sklearn.__version__)
+
+df = pd.read_csv("data/house/house.csv")
+df["loyer_m2"] = df["loyer"] / df["surface"]
+print(df)
+df.to_json("data/house/house.json", indent=2, index=False)
+df.to_excel("data/house/house.xlsx", index=False)
+
+v12 = np.arange(12, dtype=np.float64)
+print(v12)
+m34 = v12.reshape(3, 4)
+print(m34)
+print(m34 * 2)
+print(np.cos(m34))
