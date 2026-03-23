1f23b886608b01f378d4f2d9c8b0c1196b5c6739
Cyril Vincent <contact@cyrilvincent.com>
2026-03-23 14:04:31 +0100

    Orthotaxy

---
+++ demo_heart.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import sklearn.linear_model as lm
+import matplotlib.pyplot as plt
+import sklearn.pipeline as pipe
+import sklearn.preprocessing as pre
+import sklearn.model_selection as ms
+import sklearn.neighbors as n
+import numpy as np
+
+
+df = pd.read_csv("data/heartdisease/data_cleaned_up.csv")
+
+print(df.describe())
+
+y = df["num"]
+x = df.drop(["num"], axis=1)
+
+np.random.seed(42)
+
+xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, train_size=0.8, test_size=0.2)
+
+model = n.KNeighborsClassifier(n_neighbors=3)
+model.fit(xtrain, ytrain)
+
+trainscore = model.score(xtrain, ytrain)
+testscore = model.score(xtest, ytest)
+print(trainscore, testscore)
+
+
+
+
