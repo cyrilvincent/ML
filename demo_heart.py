1c27444a9fa5baf1922917bbea4a0969ec90df21
Cyril Vincent <contact@cyrilvincent.com>
2026-03-23 14:30:25 +0100

    Orthotaxy

--- demo_heart.py
+++ demo_heart.py
@@ -5,6 +5,7 @@ import sklearn.pipeline as pipe
 import sklearn.preprocessing as pre
 import sklearn.model_selection as ms
 import sklearn.neighbors as n
+import sklearn.preprocessing as pp
 import numpy as np
 
 
@@ -18,6 +19,11 @@ x = df.drop(["num"], axis=1)
 np.random.seed(42)
 
 xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, train_size=0.8, test_size=0.2)
+
+scaler = pp.RobustScaler()
+scaler.fit(xtrain)
+xtrain = scaler.transform(xtrain)
+xtest = scaler.transform(xtest)
 
 model = n.KNeighborsClassifier(n_neighbors=3)
 model.fit(xtrain, ytrain)
