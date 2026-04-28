9bd1de8fe969c1142ebf706e226afcf2a9a20fda
Cyril Vincent <contact@cyrilvincent.com>
2026-04-28 15:57:08 +0200

    CNRS

--- tp_mnist.py
+++ tp_mnist.py
@@ -6,6 +6,9 @@ import sklearn.preprocessing as pp
 import sklearn.model_selection as ms
 import numpy as np
 import sklearn.neighbors as n
+import sklearn.ensemble as rf
+import sklearn.tree as tree
+import pickle
 
 data = np.load("data/mnist/mnist.npz")
 print(data)
@@ -14,24 +17,41 @@ xtest = data["x_test"]
 ytrain = data["y_train"]
 ytest = data["y_test"]
 
+
+
 print(xtrain.shape, xtest.shape)
 
 
 np.random.seed(42)
 
 xtrain = xtrain.reshape(-1, 28*28)
+
+for pixel in xtrain[0]:
+    print(f"{pixel},", end="")
+
 xtest = xtest.reshape(-1, 28*28)
 print(xtest.shape)
 
-model = n.KNeighborsClassifier(n_neighbors=3)
+# model = n.KNeighborsClassifier(n_neighbors=3)
+model = rf.RandomForestClassifier(max_depth=6)
 model.fit(xtrain, ytrain)
+
+
 
 ypred = model.predict(xtest)
 
 train_score = model.score(xtrain, ytrain)
 test_score = model.score(xtest, ytest)
 
+with open(f"data/mnist/rf-{test_score:.2f}.pkl", "wb") as f:
+    pickle.dump(model, f)
+
 print(train_score, test_score)
+
+tree.export_graphviz(model.estimators_[0], out_file="data/mnist/tree.dot", feature_names=[str(x) for x in range(784)], class_names=[str(x) for x in range(10)])
+
+plt.imshow(model.feature_importances_.reshape(28,28))
+plt.show()
 
 xtest = xtest.reshape(-1, 28, 28)
 select = np.random.randint(xtest.shape[0], size=12)
