d7c6743f9920858a1edcd1b43dbf38a88cb07307
Cyril Vincent <contact@cyrilvincent.com>
2026-04-28 09:40:59 +0200

    CNRS

--- tp_heart.py
+++ tp_heart.py
@@ -4,6 +4,7 @@ import sklearn.linear_model as lm
 import sklearn.preprocessing as pp
 import sklearn.pipeline as pipe
 import sklearn.model_selection as ms
+import sklearn.neighbors as n
 
 df = pd.read_csv("data/heartdisease/data_cleaned_up.csv")
 pd.set_option('display.max_columns', None)
@@ -14,6 +15,16 @@ x = df.drop(["num"], axis=1)
 
 print(df.corr())
 
+np.random.seed(42)
+xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, train_size=0.8, test_size=0.2)
+
+# model = lm.LinearRegression()
+for k in range(3, 12, 2):
+    model = n.KNeighborsClassifier(n_neighbors=k)
+
+    model.fit(xtrain, ytrain)
+
+    print(model.score(xtest, ytest))
 # train_test_split
 # fit
 # predict
