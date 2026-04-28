f84e09e841994438b7d30bd2de9cc7702126b933
Cyril Vincent <contact@cyrilvincent.com>
2026-04-28 14:25:20 +0200

    CNRS

--- tp_cancer.py
+++ tp_cancer.py
@@ -7,8 +7,14 @@ import sklearn.pipeline as pipe
 import sklearn.neighbors as n
 import numpy as np
 import sklearn.model_selection as ms
+import sklearn.ensemble as rf
+import sklearn.tree as tree
+
 
 df = pd.read_csv("data/breast-cancer/data.csv")
+
+df["rnd"] = np.random.rand()
+
 df.hist(bins=10)
 plt.show()
 df["concave_points_worst"].hist(bins=20)
@@ -26,13 +32,24 @@ scaler.fit(xtrain)
 xtrain = scaler.transform(xtrain)
 xtest = scaler.transform(xtest)
 
-model = n.KNeighborsClassifier(n_neighbors=3)
+# for k in range(3, 15, 2):
+# model = n.KNeighborsClassifier(n_neighbors=k)
+model = rf.RandomForestClassifier(max_depth=6)
 model.fit(xtrain, ytrain)
 
 train_score = model.score(xtrain, ytrain)
 test_score = model.score(xtest, ytest)
 
 print(train_score, test_score)
+
+print(model.feature_importances_)
+
+plt.bar(x.columns, model.feature_importances_)
+plt.xticks(rotation=45)
+plt.show()
+
+tree.export_graphviz(model.estimators_[0], out_file="data/breast-cancer/tree.dot", feature_names=x.columns, class_names=["0","1"])
+# Graphviz Viewer
 
 ypred = model.predict(xtest)
 
