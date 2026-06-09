6164af5fae68f8e9f0ba511be585c8005819707b
cyrilvincent <contact@cyrilvincent.com>
2026-06-09 08:39:51 +0200

    CEA

--- tp_cancer.py
+++ tp_cancer.py
@@ -13,5 +13,26 @@ pd.set_option('display.max_colwidth', 500)
 df = pd.read_csv("data/breast-cancer/data.csv")
 print(df.describe())
 
-# kNN
-# Trouver la meilleur valeur de k
+y = df["diagnosis"]
+x = df.drop(["diagnosis", "id"], axis=1)
+
+np.random.seed(42)
+
+xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, train_size=0.8, test_size=0.2)
+
+scaler = pp.RobustScaler()
+scaler.fit(xtrain)
+xtrain = scaler.transform(xtrain)
+xtest = scaler.transform(xtest)
+
+for k in range(3, 12):
+    model = n.KNeighborsClassifier(n_neighbors=k)
+    model.fit(xtrain, ytrain)
+
+    print(k)
+    print(f"Train score: {model.score(xtrain, ytrain)}")
+    print(f"Test score: {model.score(xtest, ytest)}")
+
+ypred = model.predict(xtest)
+
+
