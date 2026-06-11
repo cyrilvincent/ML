deaeea30c5f692375a4bbacee7f8b33c737464f9
Cyril Vincent <contact@cyrilvincent.com>
2026-06-11 16:37:45 +0200

    Orthotaxy

--- hello.py
+++ hello.py
@@ -4,6 +4,7 @@ import sklearn.linear_model as lm
 import numpy as np
 import sklearn.pipeline as pipe
 import sklearn.preprocessing as pp
+import sklearn.model_selection as ms
 
 print("Hello")
 print(pd.__version__)
@@ -16,13 +17,15 @@ plt.scatter(df["surface"], df["loyer"])
 y = df["loyer"]
 x = df["surface"].values.reshape(-1, 1)
 
+xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, train_size=0.8, test_size=0.2)
+
 # model = lm.LinearRegression()
 model = pipe.make_pipeline(pp.PolynomialFeatures(2), lm.Ridge())
-model.fit(x, y)
+model.fit(xtrain, ytrain)
 # print(model.coef_, model.intercept_)
 
-print(model.score(x, y))
-
+print("Training Score", model.score(xtrain, ytrain))
+print("Testing Score", model.score(xtest, ytest))
 
 x = np.arange(400).reshape(-1, 1)
 ypred = model.predict(x)
