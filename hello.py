d434c41e067365f622346750cf04b604c3cac1d8
Cyril Vincent <contact@cyrilvincent.com>
2026-06-11 16:18:55 +0200

    Orthotaxy

--- hello.py
+++ hello.py
@@ -2,6 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sklearn.linear_model as lm
 import numpy as np
+import sklearn.pipeline as pipe
+import sklearn.preprocessing as pp
 
 print("Hello")
 print(pd.__version__)
@@ -11,16 +13,18 @@ print(df.describe())
 
 plt.scatter(df["surface"], df["loyer"])
 
-
 y = df["loyer"]
 x = df["surface"].values.reshape(-1, 1)
 
-model = lm.LinearRegression()
+# model = lm.LinearRegression()
+model = pipe.make_pipeline(pp.PolynomialFeatures(2), lm.Ridge())
 model.fit(x, y)
-print(model.coef_, model.intercept_)
+# print(model.coef_, model.intercept_)
+
+print(model.score(x, y))
+
 
 x = np.arange(400).reshape(-1, 1)
 ypred = model.predict(x)
-
 plt.plot(x, ypred, color="red")
 plt.show()
