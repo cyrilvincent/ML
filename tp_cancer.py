60167288aecf2b550990dc4153817b7e1680f4fe
Cyril Vincent <contact@cyrilvincent.com>
2026-06-12 10:00:27 +0200

    Orthotaxy

--- tp_cancer.py
+++ tp_cancer.py
@@ -12,11 +12,17 @@ print(df.describe())
 y = df["diagnosis"]
 x = df.drop(["id", "diagnosis"], axis=1)
 
-xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, train_size=0.8, test_size=0.2)
+xtrain, xtest, ytrain, ytest = ms.train_test_split(x, y, train_size=0.8, test_size=0.2, random_state=42)
 
-model = n.KNeighborsClassifier(n_neighbors=3)
-model.fit(xtrain, ytrain)
+scaler = pp.RobustScaler()
+scaler.fit(xtrain)
+xtrain = scaler.transform(xtrain)
+xtest = scaler.transform(xtest)
 
-print("Training Score", model.score(xtrain, ytrain))
-print("Testing Score", model.score(xtest, ytest))
+for k in range(3, 12):
+    model = n.KNeighborsClassifier(n_neighbors=k)
+    model.fit(xtrain, ytrain)
+
+    print("Training Score", model.score(xtrain, ytrain))
+    print("Testing Score", model.score(xtest, ytest))
 
---
+++ tp_mnist.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn.linear_model as lm
+import sklearn.preprocessing as pp
+import sklearn.pipeline as pipe
+import sklearn.model_selection as ms
+import numpy as np
+import sklearn.neighbors as n
+import sklearn.ensemble as rf
+import sklearn.tree as tree
+import pickle
+
+data = np.load("data/mnist/mnist.npz")
+xtrain = data["x_train"]
+xtest = data["x_test"]
+ytrain = data["y_train"]
+ytest = data["y_test"]
+
+print(xtrain.shape)
+
+xtrain = xtrain.reshape(-1, 28*28)
+xtest = xtest.reshape(-1, 28*28)
+
+model = n.KNeighborsClassifier(n_neighbors=3)
+model.fit(xtrain, ytrain)
+
+print(f"Train score: {model.score(xtrain, ytrain)}")
+print(f"Test score: {model.score(xtest, ytest)}")
+
+ypred = model.predict(xtest)
+
+xtest = xtest.reshape(-1, 28, 28)
+select = np.random.randint(xtest.shape[0], size=12)
+
+for index, value in enumerate(select):
+    plt.subplot(3, 4, index + 1)
+    plt.axis("off")
+    plt.imshow(xtest[value], cmap=plt.cm.gray_r)
+    plt.title(f"Predicted {ypred[value]}")
+plt.show()
